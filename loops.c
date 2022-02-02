275cd36196b26b67b4bb89be3e98c9b71b49eb5b
krish <krishnaveni.k@thundersoft.com>
2022-02-02 22:39:45 +0530

    loops

---
+++ loops.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+   int n=5;
+   int i;
+   while(i<n)
+   {
+       printf("%d\n",i);
+       i++;
+   }
+   for(i=0;i<n;i++)
+   {
+       printf("%d\n",i);
+   }
+   i=0;
+   do
+   {
+        printf("%d\n",i);
+        i++;
+   }while(i<n);
+    return 0;
+}
+int a=20;
+
+ if(n==a)
+   {
+       printf("both are same\n");
+   }
+if(a>n)
+{
+printf("a is big\n");
+
+}
+else
+{
+printf("n is big");
+}
+}
